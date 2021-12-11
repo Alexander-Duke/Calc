6f700d3d8e68692ab792150140dbbdc3b3c1b248
Alexander Duke <9803348@gmail.com>
2021-12-11 13:13:19 +0300

    create main loop

--- src/com/company/Calc.java
+++ src/com/company/Calc.java
@@ -22,87 +22,91 @@ public class Calc {
         );
 
         next = num.nextLine();
-
-        // 1. Ввод первого числа и его проверка
-        System.out.print("Enter first num: ");
-        while (isTrue) {
-            if (num.hasNextFloat()) {
-                first = num.nextFloat();
-                isTrue = false;
-            } else {
-                System.out.println("Incorrect value. Try again" + "\n" + "Enter first num: ");
-                isTrue = true;
-                next = num.nextLine();
-            }
-        }
-
-        // 2. Ввод операции и проверка
-        do {
-            System.out.print("Enter operation: ");
-            operation = num.next();
-
-            // Проверяем правильно ли введена операция.
-            if (operation.contains("+") ||
-                    operation.contains("-") ||
-                    operation.contains("*") ||
-                    operation.contains("/"))
-                isTrue = true;
-            else
-                System.out.println("A nonexistent operation was entered." + "\n" + "Try again.");
-        } while (!isTrue);
-
-
-        // 3. Ввод второго числа и его проверка
-        System.out.print("Enter second num: "); //введите второе число
-
-        isTrue = true;
-        while (isTrue) {
-            if (num.hasNextFloat()) {
-                second = num.nextFloat();
-                isTrue = false;
-            } else {
-                System.out.println("Incorrect value. Try again" + "\n" + "Enter second num: ");
-                isTrue = true;
-                next = num.nextLine();
-            }
-        }
-
-        //проверка деления на ноль
-        while (second == 0.0 && operation.contains("/")) {
-            System.out.print("Result is = Cannot be divided by zero!" +"\n" + "Try again" + "\n" + "Enter second num: ");
+        while (!next.contains("exit")) {
             isTrue = true;
-            //скопировано из проверки ввода второго числа
+            // 1. Ввод первого числа и его проверка
+            System.out.print("Enter first num: ");
+            while (isTrue) {
+                if (num.hasNextFloat()) {
+                    first = num.nextFloat();
+                    isTrue = false;
+                } else {
+                    System.out.println("Incorrect value. Try again" + "\n" + "Enter first num: ");
+                    isTrue = true;
+                    next = num.nextLine();
+                }
+            }
+
+            // 2. Ввод операции и проверка
+            do {
+                System.out.print("\n" +"Enter operation: ");
+                operation = num.next();
+
+                // Проверяем правильно ли введена операция.
+                if (operation.contains("+") ||
+                        operation.contains("-") ||
+                        operation.contains("*") ||
+                        operation.contains("/"))
+                    isTrue = true;
+                else
+                    System.out.println("A nonexistent operation was entered." + "\n" + "Try again.");
+            } while (!isTrue);
+
+
+            // 3. Ввод второго числа и его проверка
+            System.out.print("\n" +"Enter second num: "); //введите второе число
+
+            isTrue = true;
             while (isTrue) {
                 if (num.hasNextFloat()) {
                     second = num.nextFloat();
                     isTrue = false;
                 } else {
-                    System.out.println("Incorrect value. Try again" + "\n" + "Enter second num: ");
+                    System.out.println("Incorrect value. Try again" + "\n" + "\n" + "Enter second num: ");
                     isTrue = true;
                     next = num.nextLine();
                 }
             }
-            //скопировано из проверки ввода второго числа
-        }
+            //проверка деления на ноль
+            while (second == 0.0 && operation.contains("/")) {
+                System.out.print("Result is = Cannot be divided by zero!" + "\n" + "Try again" + "\n" + "Enter second num: ");
+                isTrue = true;
+                //скопировано из проверки ввода второго числа
+                while (isTrue) {
+                    if (num.hasNextFloat()) {
+                        second = num.nextFloat();
+                        isTrue = false;
+                    } else {
+                        System.out.println("Incorrect value. Try again" + "\n" + "Enter second num: ");
+                        isTrue = true;
+                        next = num.nextLine();
+                    }
+                }
+                //скопировано из проверки ввода второго числа
+            }
 
-        // 4. Выполнение операции
-        switch (operation) {
-            case "+":
-                result = first + second;
-                System.out.println("Result is = " + result);
-                break;
-            case "-":
-                result = first - second;
-                System.out.println("Result is = " + result);
-                break;
-            case "*":
-                result = first * second;
-                System.out.println("Result is = " + result);
-                break;
-            case "/":
-                result = first / second;
-                System.out.println("Result is = " + result);
-                break;
+            // 4. Выполнение операции
+            switch (operation) {
+                case "+":
+                    result = first + second;
+                    System.out.println("\n" + "Result is: "+ "\n" + first + " " + operation + " " + second + " = " + result);
+                    break;
+                case "-":
+                    result = first - second;
+                    System.out.println("\n" + "Result is: "+ "\n" + first + " " + operation + " " + second + " = " + result);
+                    break;
+                case "*":
+                    result = first * second;
+                    System.out.println("\n" + "Result is: "+ "\n" + first + " " + operation + " " + second + " = " + result);
+                    break;
+                case "/":
+                    result = first / second;
+                    System.out.println("\n" + "Result is: "+ "\n" + first + " " + operation + " " + second + " = " + result);
+                    break;
+            }
+            next = num.nextLine();
+            System.out.println("\n" +"Press Enter to retry or enter an \"exit\" to complete.");
+            next = num.nextLine();
         }
     }
 }
